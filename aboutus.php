803f6396490c42085cea91a0bc82ffe2d169e9ac
KevinKirchhoff <KevinKirchhoff@u.boisestate.edu>
2017-03-11 16:20:20 -0700

    added content

--- aboutus.php
+++ aboutus.php
@@ -1,7 +1,12 @@
 <html>
 <?php include("header.php");
 ?>
-
+<style>
+    h2{
+        text-align:center;
+    }    
+</style>
+<h2>A little about Study Fox</h2>
 <?php include("footer.php")
 ?>
 
--- faq.php
+++ faq.php
@@ -1,7 +1,57 @@
 <html> 
 <?php include("header.php");
 ?>
-
+    
+<style>
+    .faqheader{
+        font-size:30px;
+    }
+    .panel-group{
+        padding-top:40px;
+        padding-left:40px;
+        padding-right:40px;
+    }
+   
+</style>    
+    
+<div class="panel-group" id="accordian">
+    <div class="faqheader">General Questions</div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">
+        <a class="accordian-toggle collapsed" data-toggle="collapse" data-parent="accordian" href="#CollapseOne" aria-expanded="false">What is StudyFox?</a>
+        </h4>
+    </div>
+    <div id="CollapseOne" class="panel-collapse collapse" aria-expanded="false" style="height:0px;">
+        <div class="panel-body">
+        "   Stuff stuff stuff  "
+        </div>
+    </div>
+    </div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">
+        <a class="accordian-toggle collapsed" data-toggle="collapse" data-parent="accordian" href="#CollapseTwo" aria-expanded="false">What is this and that</a>
+        </h4>
+    </div>
+    <div id="CollapseTwo" class="panel-collapse collapse" aria-expanded="false" style="height:0px;">
+        <div class="panel-body">
+        "   Stuff stuff stuff  "
+        </div>
+    </div>
+    </div>
+   
+    
+    
+    
+    
+    
+    
+    
+    
+</div>
+    
+    
 <?php include("footer.php");
 ?>
 </html>
