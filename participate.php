08964a53740757bc8112bfc57b69aca3e8e2246a
KevinKirchhoff <KevinKirchhoff@u.boisestate.edu>
2017-03-11 11:36:19 -0700

    updated form

--- participate.php
+++ participate.php
@@ -2,6 +2,30 @@
 <? include("header.php");
 ?>
 
+<style>
+container{
+top-padding:100px;
+}
+
+
+</style>
+<body>
+<div class="container">
+<div class="form-group row">
+	<label for "name-input" class="col-2 col-form-label">Name:</label>
+	<div class="col-10">
+		<input class="form-control" type="text" value="" id="name-input">
+	</div>
+</div>
+<div class="form-group row">
+	<label for "class-input" class="col-2 col-form-label">Class:</label>
+	<div class="col-10">
+		<input class="form-control" type="text" value="" id="class-input">
+	</div>
+</div>
+</div>
+</div>
+</body>
 
 
 </html>
