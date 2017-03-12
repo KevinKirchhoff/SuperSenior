4d7ec9b39cc5d09f92217aaa2a67b20bf09d966e
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-03-11 18:27:39 -0700

    heyo

--- request.sql
+++ request.sql
@@ -2,12 +2,11 @@ CREATE TABLE request(
     id_ INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(32) DEFAULT NULL,
     class VARCHAR(32) NOT NULL,
-    idClass INT NOT NULL
+    idClass INT NOT NULL,
     availability DATE NOT NULL,
-    contactinfo NOT NULL,
-    comments DEFAULT NULL,
-    postDate DATE NOT NULL,
-    FOREIGN KEY (idClass) REFERENCES classes(idClass)
+    contactinfo VARCHAR(32) NOT NULL,
+    comments VARCHAR(32) DEFAULT NULL,
+    postDate DATE NOT NULL
 
 );
 
